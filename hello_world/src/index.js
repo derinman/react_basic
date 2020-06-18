591071b24cb9970a256339bf58b506901409ea9d
inman <sf990668852@gmail.com>
2020-06-18 14:29:03 +0800

    some basic react

--- hello_world/src/index.js
+++ hello_world/src/index.js
@@ -51,6 +51,43 @@ ReactDOM.render(
 );
 */
 
+/*
+//同上
+ReactDOM.render(
+  <h1 style = {{ fontSize: '100px', color: 'red' }} > Hello, world! </h1>,
+  document.getElementById('root')
+);
+*/
+
+/*
+//這是錯誤的寫法
+ReactDOM.render(
+  <h1 class = "title" > Hello, world! </h1>,
+  document.getElementById('root')
+);
+*/
+/*
+//這是正確的寫法
+ReactDOM.render(
+  <h1 className = "title" > Hello, world! </h1>,
+  document.getElementById('root')
+);
+*/
+
+
+const multiButton=()=>{
+  var output=[];
+  for(let i=0;i<11;++i)
+      output.push(<button>我是第{i}個按鍵</button>)
+  return output;
+}
+  
+ReactDOM.render(
+  <div>
+      { multiButton() }
+  </div>,
+  document.getElementById('root')
+);
 
 
 // If you want your app to work offline and load faster, you can change
