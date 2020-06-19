b1c8035868b081cacafb5dcfc01ce34041cf2a41
inman <sf990668852@gmail.com>
2020-06-19 16:33:37 +0800

    first commit

---
+++ class_component/my-app/src/index.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+/*
+//比較class function差異，"render"
+class App2 extends Component{ 
+  render(){
+    return(
+      <div>
+        helloWorld,hi
+      </div>
+    );
+  }
+}
+function App3(){
+  return(
+    <div>
+          helloWorld,嗨
+    </div>
+  );
+}
+ReactDOM.render(
+  <div>
+  <App2 />
+  <App3 />
+  </div>,
+  document.getElementById('root')
+);
+*/
+
+/*
+//小按鈕component
+class App2 extends Component{ 
+  render(){
+    return(
+      <button>
+        {this.props.name}
+      </button>
+    );
+  }
+}
+//注意!:<App2 name={"你敢按?"}/>
+//要加{}
+ReactDOM.render(
+  <div>
+  <App2 name={"你敢按?"}/>
+  <App2 name={"我會怕!"}/>
+  </div>,
+  document.getElementById('root')
+);
+*/
+
+
+
+serviceWorker.unregister();
